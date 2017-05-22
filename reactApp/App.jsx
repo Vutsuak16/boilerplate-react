854d58382f0b1f9f464f338766f75c5f3f80a520
Deolal <vutsuak96@gmail.com>
2017-05-22 12:05:16 +0530

    code added

---
+++ reactApp/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class App extends React.Component {
+   render() {
+      return (
+      <div>
+      	 <h1>Header</h1>
+      	 <h2>Contents</h2>
+      	 <p> This is content </p>
+      	 <ul>
+      	 <li>	kaja  </li>
+      	 <li>	baja  </li>
+      	 <li>	raja  </li>
+      	 <li>	aaja  </li>
+      	 </ul>
+         <div>
+            Hello World!!!
+         </div>
+        </div>
+      );
+   }
+}
+
+export default App;
